95f431787680a7422aac812dd2a4acc9ff1129b3
John Vinh <john.michael.vinh2@gmail.com>
2026-05-04 22:25:33 -0400

    frontend: Create proxy to backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,9 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    proxy: {
+      '/links': 'http://localhost:3000',
+    },
+  },
 })
